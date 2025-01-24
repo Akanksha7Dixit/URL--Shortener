54c1711ac47588ffd8a322a9da8922f0251a40d6
Akanksha7Dixit <akadixit98@gmail.com>
2025-01-24 19:42:09 +0530

    url shortener

---
+++ url.js
@@ -0,0 +1,36 @@
+import { readFile } from "fs/promises";
+import { createServer } from "http";
+import path from "path";
+
+const PORT = 3002;
+
+const serveFile = async (res, filepath, contentType) => {
+    try {
+        const data = await readFile(filepath);
+        res.writeHead(200, { "Content-Type": contentType });
+        res.end(data);
+    } catch {
+        res.writeHead(404, { "Content-Type": contentType });
+        res.end("404 Page not found");
+    }
+
+}
+
+const server = createServer(async (req, res) => {
+    console.log(req.url);
+
+    if (req.method === "GET") {
+        if (req.url === "/") {
+            return serveFile(res, path.join("public", "index.html"), "text/html");
+        }
+        else if (req.method === "GET") {
+            if (req.url === "/style.css") {
+                return serveFile(res, path.join("public", "style.css"), "text/css");
+            }
+        }
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+})
